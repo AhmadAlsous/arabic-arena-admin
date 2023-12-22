aa0e3cdbe8078012fb905e0bfad40521991f2687
Ahmad Alsous <alsous@amazon.com>
2023-12-22 19:25:13 +0300

    test build editor

--- src/sections/lessons/LessonTextForm.jsx
+++ src/sections/lessons/LessonTextForm.jsx
@@ -1,7 +1,11 @@
 import FormContainer from 'src/components/FormContainer';
 import styled from '@emotion/styled';
+import { Editor } from 'react-draft-wysiwyg';
+import '../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
-import * as Editor from 'ckeditor5-custom-build';
+import { EditorState } from 'draft-js';
+// import * as Editor from 'ckeditor5-custom-build';
+import { useState } from 'react';
 
 const EditorContainer = styled.div`
   width: 100%;
@@ -17,18 +21,25 @@ const EditorContainer = styled.div`
 `;
 
 function LessonTextForm({ setValue, lesson }) {
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+
+  const onEditorStateChange = (newState) => {
+    setEditorState(newState);
+    // You can also perform other actions here, such as saving the content
+  };
   const isNew = lesson === null;
   return (
     <FormContainer title="Lesson Content">
       <EditorContainer>
-        <CKEditor
+        {/* <CKEditor
           editor={Editor}
           data={isNew ? '<p>Enter lesson content here...</p>' : lesson.text}
           onChange={(_, editor) => {
             const data = editor.getData();
             setValue('text', data);
           }}
-        />
+        /> */}
+        <Editor editorState={editorState} onEditorStateChange={onEditorStateChange} />
       </EditorContainer>
     </FormContainer>
   );
