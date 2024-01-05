165e262fb75ac1703af3a03db93080a5017f5525
Ahmad Alsous <alsous@amazon.com>
2024-01-05 15:12:03 +0300

    test login logic

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -9,99 +9,107 @@ const tranformUrl = (url) => {
   return `https://www.youtube.com/embed/${videoId}`;
 };
 
-const toolbarOptions = {
-  options: [
-    'inline',
-    'blockType',
-    'fontSize',
-    'fontFamily',
-    'list',
-    'textAlign',
-    'colorPicker',
-    'link',
-    'embedded',
-    'image',
-    'history',
-  ],
-  inline: {
-    options: ['bold', 'italic', 'underline', 'strikethrough', 'superscript', 'subscript'],
-  },
-  blockType: {
-    inDropdown: true,
-    options: ['Normal', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'Blockquote', 'Code'],
-  },
-  fontSize: {
-    options: [8, 9, 10, 11, 12, 14, 16, 18, 20, 24, 30, 36, 48, 60, 72, 96],
-  },
-  fontFamily: {
-    options: ['Arial', 'Georgia', 'Impact', 'Tahoma', 'Times New Roman', 'Verdana'],
-  },
-  list: {
-    inDropdown: false,
-    options: ['unordered', 'ordered'],
-  },
-  textAlign: {
-    inDropdown: false,
-    options: ['left', 'center', 'right', 'justify'],
-  },
-  colorPicker: {
-    colors: [
-      'rgb(97,189,109)',
-      'rgb(26,188,156)',
-      'rgb(84,172,210)',
-      'rgb(44,130,201)',
-      'rgb(147,101,184)',
-      'rgb(71,85,119)',
-      'rgb(204,204,204)',
-      'rgb(65,168,95)',
-      'rgb(0,168,133)',
-      'rgb(61,142,185)',
-      'rgb(41,105,176)',
-      'rgb(85,57,130)',
-      'rgb(40,50,78)',
-      'rgb(0,0,0)',
-      'rgb(247,218,100)',
-      'rgb(251,160,38)',
-      'rgb(235,107,86)',
-      'rgb(226,80,65)',
-      'rgb(163,143,132)',
-      'rgb(239,239,239)',
-      'rgb(255,255,255)',
-      'rgb(250,197,28)',
-      'rgb(243,121,52)',
-      'rgb(209,72,65)',
-      'rgb(184,49,47)',
-      'rgb(124,112,107)',
-      'rgb(209,213,216)',
-    ],
-  },
-  link: {
-    inDropdown: false,
-    showOpenOptionOnHover: true,
-    defaultTargetOption: '_self',
-    options: ['link'],
-  },
-  embedded: {
-    embedCallback: tranformUrl,
-    defaultSize: {
-      height: '500px',
-      width: '100%',
-    },
-  },
-  image: {
-    urlEnabled: true,
-    alignmentEnabled: true,
-    previewImage: false,
-    inputAccept: 'image/gif,image/jpeg,image/jpg,image/png,image/svg',
-    alt: { present: false, mandatory: false },
-  },
-  history: {
-    inDropdown: false,
-    options: ['undo', 'redo'],
-  },
-};
-
 function Editor({ setValue, text, isVideo }) {
+  const toolbarVideoOptions = {
+    options: [
+      'inline',
+      'blockType',
+      'fontSize',
+      'fontFamily',
+      'list',
+      'textAlign',
+      'colorPicker',
+      'link',
+      'embedded',
+      'image',
+      'history',
+    ],
+    inline: {
+      options: ['bold', 'italic', 'underline', 'strikethrough', 'superscript', 'subscript'],
+    },
+    blockType: {
+      inDropdown: true,
+      options: ['Normal', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'Blockquote', 'Code'],
+    },
+    fontSize: {
+      options: [8, 9, 10, 11, 12, 14, 16, 18, 20, 24, 30, 36, 48, 60, 72, 96],
+    },
+    fontFamily: {
+      options: ['Arial', 'Georgia', 'Impact', 'Tahoma', 'Times New Roman', 'Verdana'],
+    },
+    list: {
+      inDropdown: false,
+      options: ['unordered', 'ordered'],
+    },
+    textAlign: {
+      inDropdown: false,
+      options: ['left', 'center', 'right', 'justify'],
+    },
+    colorPicker: {
+      colors: [
+        'rgb(97,189,109)',
+        'rgb(26,188,156)',
+        'rgb(84,172,210)',
+        'rgb(44,130,201)',
+        'rgb(147,101,184)',
+        'rgb(71,85,119)',
+        'rgb(204,204,204)',
+        'rgb(65,168,95)',
+        'rgb(0,168,133)',
+        'rgb(61,142,185)',
+        'rgb(41,105,176)',
+        'rgb(85,57,130)',
+        'rgb(40,50,78)',
+        'rgb(0,0,0)',
+        'rgb(247,218,100)',
+        'rgb(251,160,38)',
+        'rgb(235,107,86)',
+        'rgb(226,80,65)',
+        'rgb(163,143,132)',
+        'rgb(239,239,239)',
+        'rgb(255,255,255)',
+        'rgb(250,197,28)',
+        'rgb(243,121,52)',
+        'rgb(209,72,65)',
+        'rgb(184,49,47)',
+        'rgb(124,112,107)',
+        'rgb(209,213,216)',
+      ],
+    },
+    history: {
+      inDropdown: false,
+      options: ['undo', 'redo'],
+    },
+  };
+
+  const toolbarOptions = {
+    ...toolbarVideoOptions,
+    link: {
+      inDropdown: false,
+      showOpenOptionOnHover: true,
+      defaultTargetOption: '_self',
+      options: ['link'],
+    },
+    embedded: {
+      embedCallback: tranformUrl,
+      defaultSize: {
+        height: '500px',
+        width: '100%',
+      },
+    },
+    image: {
+      urlEnabled: true,
+      alignmentEnabled: true,
+      previewImage: false,
+      inputAccept: 'image/gif,image/jpeg,image/jpg,image/png,image/svg',
+      alt: { present: false, mandatory: false },
+    },
+    history: {
+      inDropdown: false,
+      options: ['undo', 'redo'],
+    },
+  };
+
   const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
 
   useEffect(() => {
@@ -117,13 +125,13 @@ function Editor({ setValue, text, isVideo }) {
   const onEditorStateChange = (newState) => {
     setEditorState(newState);
     const rawContentState = convertToRaw(newState.getCurrentContent());
-    setValue(isVideo ? 'videoText' : 'text', rawContentState);
+    setValue(isVideo ? 'videoText' : 'text', rawContentState, { shouldDirty: true });
   };
 
   return (
     <TextEditor
       editorState={editorState}
-      toolbar={toolbarOptions}
+      toolbar={isVideo ? toolbarVideoOptions : toolbarOptions}
       onEditorStateChange={onEditorStateChange}
       editorStyle={{
         border: '1px solid #ccc',
--- src/sections/lessons/LessonVideoForm.jsx
+++ src/sections/lessons/LessonVideoForm.jsx
@@ -91,7 +91,7 @@ function LessonVideoForm({ register, errors, setValue, watch, getValues, control
                   required: 'This field is required',
                   pattern: {
                     value:
-                      /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.?be|edpuzzle\.com\/media)\/[a-zA-Z0-9_-]+$/,
+                      /^(https?:\/\/)?(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/)[\w-]+(&[\w-]+)*$/,
                     message: 'Please enter a valid YouTube or Edpuzzle video link',
                   },
                 })}
